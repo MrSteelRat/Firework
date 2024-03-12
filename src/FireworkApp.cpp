c976b8076b38aa5f85f1fe696e4cc94afb2164e9
MrSteelRat <drebin341@gmail.com>
2024-03-12 15:23:18 +0300

    Added new logic to FWApp for particles from the Particle class

--- src/FireworkApp.cpp
+++ src/FireworkApp.cpp
@@ -1,40 +1,55 @@
-#include "FireworkApp.h"
+﻿#include "FireworkApp.h"
 
+#include <execution>
 #include <iostream>
+#include <ranges>
+#include <span>
 #include <sstream>  // For string manipulation
 
 #include "MenuWindow.h"
 
+namespace
+{
+std::atomic<float> globalTime;
+std::atomic<bool> workerMustExit;
+// std::atomic<size_t> gEffectSCount;
+// std::atomic<size_t> gParticlesCount;
+
+constexpr size_t MAX_EFFECTS = 2048;
+constexpr size_t MAX_PARTICLES_PER_EFFECT = 64;
+
+bool isParticleDead(const Particle& particle) { return !particle.isAlive(); }
+bool areFireworksDead(const std::span<Particle> fireworks)
+{
+  return fireworks.empty();
+}
+}  // namespace
+
 FireworksApp::FireworksApp(int width, int height)
     : window(sf::VideoMode(width, height), "Fireworks"),
-      particleCount(0),
-      fireworkCount(0)
+      renderFireworksPtr1(&renderFireworks1),
+      renderFireworksPtr2(&renderFireworks2),
+      renderFireworksPtr3(&renderFireworks3)
 {
   window.setFramerateLimit(60);
 
   // Load font for displaying FPS
   if (!font.loadFromFile("arial.ttf"))
   {
-    std::cerr << "Arial_not found" << std::endl;
-    ;
+    std::cerr << "Arial_not found" << '\n';
   }
-  // Set up FPS text
   fpsText.setFont(font);
   fpsText.setCharacterSize(20);
   fpsText.setFillColor(sf::Color::White);
   fpsText.setPosition(10.f, 10.f);
 
-  // Set up particle count text
-  particleCountText.setFont(font);
-  particleCountText.setCharacterSize(20);
-  particleCountText.setFillColor(sf::Color::White);
-  particleCountText.setPosition(10.f, 40.f);
+  std::thread workerThread([this] { mainUpdateLoop(); });
+  workerThread.detach();
 
-  // Set up firework count text
-  fireworkCountText.setFont(font);
-  fireworkCountText.setCharacterSize(20);
-  fireworkCountText.setFillColor(sf::Color::White);
-  fireworkCountText.setPosition(10.f, 70.f);
+  updateFireworks.reserve(MAX_PARTICLES_PER_EFFECT * MAX_EFFECTS);
+  renderFireworks1.reserve(MAX_PARTICLES_PER_EFFECT * MAX_EFFECTS);
+  renderFireworks2.reserve(MAX_PARTICLES_PER_EFFECT * MAX_EFFECTS);
+  renderFireworks3.reserve(MAX_PARTICLES_PER_EFFECT * MAX_EFFECTS);
 }
 
 void FireworksApp::run()
@@ -48,10 +63,11 @@ void FireworksApp::run()
     {
       while (window.isOpen())  // Game loop
       {
+        const float deltaTime = clock.restart().asSeconds();
+        globalTime.fetch_add(deltaTime);
         processEvents();
-        update();
-        render();
-        updateFPS();
+        render(deltaTime);
+        std::this_thread::yield();
       }
     }
     else if (result == MenuWindow::MenuResult::Exit)
@@ -64,6 +80,7 @@ void FireworksApp::run()
 void FireworksApp::processEvents()
 {
   sf::Event event;
+  std::vector<sf::Vector2f> newParticles;
   while (window.pollEvent(event))
   {
     if (event.type == sf::Event::Closed)
@@ -74,9 +91,8 @@ void FireworksApp::processEvents()
     {
       if (event.mouseButton.button == sf::Mouse::Left)
       {
-        fireworks.push_back(Firework(event.mouseButton.x, event.mouseButton.y));
-        particleCount += 1;  // Increment particle count
-        fireworkCount += 1;  // Increment firework count
+        newParticles.emplace_back(static_cast<float>(event.mouseButton.x),
+                                  static_cast<float>(event.mouseButton.y));
       }
     }
     else if (event.type == sf::Event::KeyPressed)
@@ -97,48 +113,152 @@ void FireworksApp::processEvents()
       }
     }
   }
+
+  addParticles(newParticles);
 }
 
-void FireworksApp::update()
+void FireworksApp::update(float deltaTime)
 {
-  for (size_t i = 0; i < fireworks.size(); ++i)
-  {
-    fireworks[i].update();  // Correct usage: No arguments should be passed
-    if (!fireworks[i].isAlive())
+  if (newParticleMutex.try_lock())
+  {  // add new particles safely
+    auto newParticlePositions = mNewParticlePositions;
+    mNewParticlePositions.clear();
+    newParticleMutex.unlock();
+
+    for (const auto& position :
+         newParticlePositions |
+             std::views::take_while(
+                 [this](auto) { return updateFireworks.size() < MAX_EFFECTS; }))
     {
-      fireworks.erase(fireworks.begin() + i);
-      --i;
+      std::vector<Particle> particles;
+      particles.reserve(MAX_PARTICLES_PER_EFFECT);
+      for (size_t i = 0; i < MAX_PARTICLES_PER_EFFECT; ++i)
+      {
+        particles.emplace_back(position.x, position.y);
+      }
+      updateFireworks.emplace_back(std::move(particles));
+    }
+  }
+
+  std::for_each(std::execution::par_unseq,
+                updateFireworks.begin(),
+                updateFireworks.end(),
+                [deltaTime](auto& firework)
+                {
+                  for (auto& particle : firework)
+                  {
+                    particle.update(deltaTime);
+                  }
+
+                  const auto ret =
+                      std::ranges::remove_if(firework, isParticleDead);
+                  firework.erase(ret.begin(), ret.end());
+                });
+
+  const auto ret = std::ranges::remove_if(updateFireworks, areFireworksDead);
+  updateFireworks.erase(ret.begin(), ret.end());
+
+  renderFireworksPtr1->assign(updateFireworks.begin(), updateFireworks.end());
+
+  if (swapFireworksMutex.try_lock())
+  {
+    std::swap(renderFireworksPtr1, renderFireworksPtr2);
+    swapFireworksMutex.unlock();
+  }
+}
+
+void FireworksApp::mainUpdateLoop()
+{
+  while (!workerMustExit)
+  {
+    try
+    {
+      static float lastTime = 0;
+      const auto time = globalTime.load();
+      const float delta = time - lastTime;
+      lastTime = time;
+
+      if (delta > 0)
+      {
+        update(delta);
+      }
+
+      const int deltaMs = delta / 1000;
+
+      static constexpr int MIN_UPDATE_PERIOD_MS = 10;
+      if (deltaMs < MIN_UPDATE_PERIOD_MS)
+      {
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(MIN_UPDATE_PERIOD_MS - deltaMs));
+      }
+    }
+    catch (std::exception& e)
+    {
+      std::stringstream stream;
+      stream << e.what() << '\n';
+      std::cerr << stream.str();
+    }
+    catch (...)
+    {
+      // Что-то пошло не так
     }
   }
 }
 
-void FireworksApp::render()
+void FireworksApp::render(float deltaTime)
 {
   window.clear();
-  for (const auto& firework : fireworks)
+  // std::unique_lock lock(swapFireworksMutex);
+  // std::swap(renderFireworksPtr2, renderFireworksPtr3);
+  // lock.unlock();
+  if (swapFireworksMutex.try_lock())
   {
-    sf::CircleShape particle(2.f);
-    particle.setFillColor(firework.getColor());
-    particle.setPosition(firework.getPosition());
-    window.draw(particle);
+    std::swap(renderFireworksPtr2, renderFireworksPtr3);
+    swapFireworksMutex.unlock();
   }
-  window.draw(fpsText);               // Draw FPS counter
-  std::stringstream particleCountSS;  // Draw particle count
-  particleCountSS << "Particles: " << particleCount;
-  particleCountText.setString(particleCountSS.str());
-  window.draw(particleCountText);  // Draw firework count
-  std::stringstream fireworkCountSS;
-  fireworkCountSS << "Fireworks: " << fireworkCount;
-  fireworkCountText.setString(fireworkCountSS.str());
-  window.draw(fireworkCountText);
+  for (const auto& firework : *renderFireworksPtr3)
+  {
+    for (const auto& particle : firework)
+    {
+      sf::CircleShape dot(1.f);
+      dot.setFillColor(particle.getColor());
+      dot.setPosition(particle.getPosition());
+      window.draw(dot);
+
+      for (const auto& [position, color] : particle.getTail())
+      {
+        dot.setFillColor(color);
+        dot.setPosition(position);
+        window.draw(dot);
+      }
+    }
+  }
+  // lock.unlock();
+
+  // Calculate FPS and update FPS text
+  float fps = 1.f / deltaTime;
+  std::wstringstream ss;
+  ss << L"FPS: " << static_cast<int>(fps) << "\n";
+  ss << L"Firework: " << renderFireworksPtr3->size() << "\n";
+
+  auto particlesCount = std::transform_reduce(std::execution::par_unseq,
+                                              renderFireworksPtr3->begin(),
+                                              renderFireworksPtr3->end(),
+                                              size_t(0),
+                                              std::plus{},
+                                              [](std::vector<Particle>& val)
+                                              { return val.size(); });
+  ss << L"Particle: " << particlesCount << "\n";
+  fpsText.setString(ss.str());
+
+  window.draw(fpsText);  // Draw FPS counter
   window.display();
 }
 
-void FireworksApp::updateFPS()
+void FireworksApp::addParticles(std::span<sf::Vector2f> newParticlePositions)
 {
-  // Calculate FPS and update FPS text
-  float fps = 1.f / clock.restart().asSeconds();
-  std::stringstream ss;
-  ss << "FPS: " << static_cast<int>(fps);
-  fpsText.setString(ss.str());
+  std::unique_lock lock(newParticleMutex);
+  mNewParticlePositions.insert(mNewParticlePositions.end(),
+                               newParticlePositions.begin(),
+                               newParticlePositions.end());
 }
--- src/FireworkApp.h
+++ src/FireworkApp.h
@@ -2,9 +2,13 @@
 #define FIREWORKSAPP_H
 
 #include <SFML/Graphics.hpp>
+#include <condition_variable>
+#include <functional>
+#include <span>
+#include <thread>
 #include <vector>
 
-#include "Firework.h"
+#include "Particle.h"
 
 class FireworksApp
 {
@@ -15,21 +19,26 @@ class FireworksApp
 
  private:
   sf::RenderWindow window;
-  std::vector<Firework> fireworks;
+  std::vector<std::vector<Particle>> updateFireworks;
+  std::vector<std::vector<Particle>> renderFireworks1;
+  std::vector<std::vector<Particle>> renderFireworks2;
+  std::vector<std::vector<Particle>> renderFireworks3;
+  std::vector<std::vector<Particle>>* renderFireworksPtr1;
+  std::vector<std::vector<Particle>>* renderFireworksPtr2;
+  std::vector<std::vector<Particle>>* renderFireworksPtr3;
+  std::vector<sf::Vector2f> mNewParticlePositions;
+  std::mutex swapFireworksMutex;
+  std::mutex newParticleMutex;
 
   sf::Clock clock;             // Add clock for measuring time
   sf::Font font;               // Font for displaying FPS
   sf::Text fpsText;            // Text for displaying FPS
-  sf::Text particleCountText;  // Text for displaying particle count
-  sf::Text fireworkCountText;  // Text for displaying firework count
-
-  int particleCount;
-  int fireworkCount;
 
   void processEvents();
-  void update();
-  void render();
-  void updateFPS();
+  void update(float deltaTime);
+  void mainUpdateLoop();
+  void render(float deltaTime);
+  void addParticles(std::span<sf::Vector2f> newParticlePositions);
 };
 
 #endif
